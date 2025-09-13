341cb6aa40f38925938299afbca46ebf6b5ea1d4
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2025-09-13 13:40:23 +0000

    first

--- 01basicsreact/src/App.js
+++ 01basicsreact/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Chai aur react | ARYAN </h1>
+    /*
+    cd_________ {this syntex use for involve into the file}
+    npm ---> node packeg maneger
+    npx ---> node packeg exicuter
+    Use keyword [dir] for check the packeg of json
+    Command-- > [npm run start(for exicution without vite)]
+                [npm run build(for create a build folder)]
+
+                [Main command: npm create vite@latest(This command use for create a file into the vscode)]
+                [npm install (Use for create node modules into folder)]
+    */
   );
 }
 
