361854f5949dd53261187427b7db6d06827d179a
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2025-09-15 17:51:56 +0000

    chai

--- 01basicsreact/src/App.js
+++ 01basicsreact/src/App.js
@@ -1,21 +1,32 @@
 
+import Chai from './chai'
 
 function App() {
   return (
+    <>
+    <Chai />
     <h1>Chai aur react | ARYAN </h1>
-    /*
-    cd_________ {this syntex use for involve into the file}
-    npm ---> node packeg maneger
-    npx ---> node packeg exicuter
-    Use keyword [dir] for check the packeg of json
-                [cd ..]{this is use for outer to one folder}
-    Command-- > [npm run start(for exicution without vite)]
-                [npm run build(for create a build folder)]
-
-                [Main command: npm create vite@latest(This command use for create a file into the vscode)]
-                [npm install (Use for create node modules into folder)]
-    */
+    </>
   );
 }
+ export default App;
 
-export default App;
+// function App() {
+//   return (
+//     <h1>Chai aur react | ARYAN </h1>
+//     /*
+//     cd_________ {this syntex use for involve into the file}
+//     npm ---> node packeg maneger
+//     npx ---> node packeg exicuter
+//     Use keyword [dir] for check the packeg of json
+//                 [cd ..]{this is use for outer to one folder}
+//     Command-- > [npm run start(for exicution without vite)]
+//                 [npm run build(for create a build folder)]
+
+//                 [Main command: npm create vite@latest(This command use for create a file into the vscode)]
+//                 [npm install (Use for create node modules into folder)]
+//     */
+//   );
+// }
+
+// export default App;
