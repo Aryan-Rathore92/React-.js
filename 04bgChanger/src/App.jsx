25a8e50c7e62c1679d8f24a4bc57ffc44e470b6c
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2025-10-11 12:27:24 +0000

    app js

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react"
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setcolor] = useState("olive")
 
   return (
-
-    // <div className="w-full" h-screen duration-200"
-    // style ={{backgroundColor: color}} ></div>
-
-
+  <div className="w-full h-screen duration-200"
+  style = {{backgroundColor: color}}
+  >
+     <div className="fixed-flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2"></div>
+      test</div> 
+  </div>
 
   )
 }
