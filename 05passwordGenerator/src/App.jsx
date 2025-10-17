961e7879cb8c215c981b84d92d4398c7bcc42889
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2025-10-17 14:37:43 +0000

    App.js

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 
 
 
@@ -15,7 +15,7 @@ function App() {
       if(numberAllowed) str+= "0123456789"
       if(charAllowed) str+= "!@#$%^&*-_+=[]{}~`"
 
-      for (let i = 0; i <= array.length; i++) {
+      for (let i = 0; i <= length; i++) {
         
         let char = Math.floor(Math.random() * str.length + 1)
         pass = str.charAt(char);
@@ -23,6 +23,10 @@ function App() {
 
       setPassword(pass)
   },[length, numberAllowed, charAllowed, setPassword])
+
+  useEffect(() => {
+       passwordGenerator()
+  }, [length, numberAllowed, charAllowed]);
 
   return (
     <>
@@ -45,7 +49,11 @@ function App() {
           </div>
 
           <div className='flex text-sm gap-x-2'></div>
+
           <div className='flex items-center gap-x-1 '>
+            <style>
+
+            </style>
             <input
             className="accent-blue-700"
             color='blue'
@@ -53,11 +61,12 @@ function App() {
             min={6}
             max={100}
             value={length}
-            // className='cursor-pointer'
+            //  className='cursor-pointer'
             onChange={(e) => {setLength(e.target.value)}}
             />
-            <label>Length : {length}</label>
-          </div>
+            
+            <label> Length : {length}</label>
+          </div> 
 
           <div className='flex items-center gap-x-1'>
             <input 
@@ -71,6 +80,7 @@ function App() {
             />
             <label htmlFor="numberInput">Numbers</label>
           </div>
+         
 
           <div className='flex items-center gap-x-1'>
             <input 
