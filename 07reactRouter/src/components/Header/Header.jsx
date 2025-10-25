cf1a31165d2b5209b98058b1d73956f0fe4f2190
Aryan-Rathore92 <aryanrathore58566@gmail.com>
2025-10-25 05:01:52 +0000

    react router

--- 07reactRouter/src/components/Header/Header.jsx
+++ 07reactRouter/src/components/Header/Header.jsx
@@ -17,17 +17,18 @@ export default function Header() {
                     <div className="flex items-center lg:order-2">
                         <Link
                             to="#"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4
+                            className="text-gray-800 focus:ring-4
                              focus:ring-gray-300 font-medium rounded-lg text-sm px-4
-                              lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                              lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none hover:bg-orange-700 transition-3s hover:text-white
+                              duration-200 ease-in-out"
                         >
                             Log in
                         </Link>
                         <Link
                             to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800
+                            className="text-white bg-orange-700 hover:bg-orange-600
                              focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4
-                              lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                              lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none "
                         >
                             Get started
                         </Link>
